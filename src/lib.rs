acc315edbb6ff3d6e8ed941499ecbf365cd83462
liwuhou <hugewilliam@foxmail.com>
2024-04-30 20:12:17 +0800

    feat: mvp

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,23 @@ impl Cli {
 
         match opts.subcmd {
             SubCmd::Run(args) => {
-                let script = utils::get_script(args.script);
-                Ok(utils::run_script(&script)?)
+                let script = utils::get_alias(args.alias_name);
+                Ok(utils::run_alias_script(&script)?)
             }
-            SubCmd::Alias(args) => Ok(true),
-            SubCmd::Delete(args) => Ok(true),
+            SubCmd::Alias(args) => {
+                let alias = utils::get_alias(args.alias_name);
+                let script = args.script;
+
+                Ok(utils::set_alias_script(&alias, script)?)
+            }
+            SubCmd::Delete(args) => {
+                let delete_script = utils::get_alias(args.alias_name);
+                Ok(utils::delete_alias_script(&delete_script)?)
+            }
+            SubCmd::Show(args) => {
+                let is_show_all = args.all.unwrap_or_default();
+                Ok(utils::show_alias(is_show_all)?)
+            }
         }
     }
 }
@@ -31,24 +43,42 @@ pub struct Args {
 pub enum SubCmd {
     /// Run any alias script or default
     Run(Run),
-    // Set alias to map script
+    /// Set alias to map script
     Alias(Alias),
-    // Delete alias
+    /// Delete alias
     Delete(Delete),
+    /// Show alias
+    Show(Show),
 }
 
 #[derive(Debug, Parser)]
 pub struct Run {
-    pub script: Option<String>,
+    pub alias_name: Option<String>,
 }
 
 #[derive(Debug, Parser)]
 pub struct Alias {
-    pub alias_name: String,
-    pub scripts: Vec<String>,
+    pub alias_name: Option<String>,
+    pub script: Option<String>,
 }
 
 #[derive(Debug, Parser)]
 pub struct Delete {
-    pub alias_name: String,
+    pub alias_name: Option<String>,
+}
+
+#[derive(Debug, Parser)]
+pub struct Show {
+    #[arg(
+        short, 
+        long, 
+        num_args = 0..=1, 
+        default_missing_value = "true", 
+        value_parser = parser_show_params
+    )]
+    pub all: Option<bool>,
+}
+
+fn parser_show_params(is_show_all: &str) -> Result<bool> {
+    Ok(!is_show_all.is_empty() && !is_show_all.eq("false"))
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 extern crate dirs;
 extern crate ini;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use colored::*;
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::FuzzySelect;
@@ -15,6 +15,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
+
+const DEFAULT_SCRIPT: &str = "-";
 
 pub enum State {
     Success,
@@ -53,11 +55,7 @@ pub struct PackageInfo {
     pub scripts: Option<HashMap<String, String>>,
 }
 
-pub fn get_script(script: Option<String>) -> String {
-    script.map_or(String::from("__default"), |x| x)
-}
-
-pub fn execute(command: &str, args: &Vec<&str>) -> Result<bool> {
+fn execute(command: &str, args: &Vec<&str>) -> Result<bool> {
     let runner = Command::new(command)
         .args(args)
         .stdout(Stdio::piped())
@@ -74,50 +72,6 @@ pub fn execute(command: &str, args: &Vec<&str>) -> Result<bool> {
         }
     }
     Ok(true)
-}
-
-pub fn run_script(script: &str) -> Result<bool> {
-    let mut config = get_alias_ini()?;
-    let current_path = env::current_dir()?
-        .into_os_string()
-        .into_string()
-        .expect("Fail to convert the path");
-
-    let package_json = PathBuf::from(&current_path).join("package.json");
-
-    if !package_json.exists() {
-        console(State::Error, "Current directory is not a node project!");
-        return Ok(false);
-    }
-
-    match config.with_section(Some(script)).get(&current_path) {
-        Some(script) => Ok(execute("npm", &vec!["run", script])?),
-        None => {
-            let scripts_in_packagejson = get_scripts(&package_json)?;
-            let selected_index = FuzzySelect::with_theme(&ColorfulTheme::default())
-                .with_prompt("Pick up the script that you want to run.")
-                .default(0)
-                .highlight_matches(false)
-                .items(
-                    &scripts_in_packagejson
-                        .iter()
-                        .map(|t| t.0.clone())
-                        .collect::<Vec<String>>(),
-                )
-                .interact()?;
-
-            let selected_script = &scripts_in_packagejson.get(selected_index).unwrap().1;
-            config.with_section(Some(script)).set(
-                &current_path,
-                selected_script.clone().normal().clear().to_string(),
-            );
-            save_alias_ini(config)?;
-            Ok(execute(
-                "npm",
-                &vec!["run", &selected_script.normal().clear()],
-            )?)
-        }
-    }
 }
 
 fn get_scripts(packagejson_path: &PathBuf) -> Result<Vec<(String, String)>> {
@@ -142,7 +96,7 @@ fn get_config_path() -> PathBuf {
         .join("runner.ini")
 }
 
-fn get_alias_ini() -> Result<Ini> {
+fn get_config_ini() -> Result<Ini> {
     let config_path = get_config_path();
 
     Ok(if !config_path.exists() {
@@ -158,4 +112,152 @@ fn get_alias_ini() -> Result<Ini> {
 
 fn save_alias_ini(config: Ini) -> Result<()> {
     Ok(config.write_to_file(get_config_path())?)
+}
+
+fn pwd() -> Result<String> {
+    Ok(env::current_dir()?.to_string_lossy().to_string())
+}
+
+fn get_project_info() -> Result<(String, PathBuf)> {
+    let pwd = pwd()?;
+    let pakcage_json = PathBuf::from(&pwd).join("package.json");
+
+    if !pakcage_json.exists() {
+        console(State::Error, "Current directory is not a node project!");
+        return Err(anyhow!("error"));
+    }
+
+    Ok((pwd, pakcage_json))
+}
+
+fn get_selected_script(package_json: PathBuf) -> Result<String> {
+    let scripts_in_packagejson = get_scripts(&package_json)?;
+    let selected_index = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick up the script that you want to run.")
+        .default(0)
+        .highlight_matches(false)
+        .items(
+            &scripts_in_packagejson
+                .iter()
+                .map(|t| t.0.clone())
+                .collect::<Vec<String>>(),
+        )
+        .interact()?;
+    let selected_script = &scripts_in_packagejson.get(selected_index).unwrap().1;
+    Ok(selected_script.to_owned())
+}
+
+pub fn get_alias(script: Option<String>) -> String {
+    match script {
+        None => DEFAULT_SCRIPT.to_owned(),
+        Some(x) => x,
+    }
+}
+
+pub fn run_alias_script(script: &str) -> Result<bool> {
+    let (current_path, package_json) = get_project_info()?;
+    let mut config = get_config_ini()?;
+
+    match config.with_section(Some(script)).get(&current_path) {
+        Some(script) => Ok(execute("npm", &vec!["run", script])?),
+        None => {
+            let selected_script = get_selected_script(package_json)?;
+            config
+                .with_section(Some(script))
+                .set(&current_path, &selected_script);
+            save_alias_ini(config)?;
+            Ok(execute("npm", &vec!["run", &selected_script])?)
+        }
+    }
+}
+
+pub fn set_alias_script(alias: &str, script: Option<String>) -> Result<bool> {
+    let (current_path, package_json) = get_project_info()?;
+    let mut config = get_config_ini()?;
+
+    let script = match script {
+        Some(script) => script,
+        None => get_selected_script(package_json)?,
+    };
+
+    config.with_section(Some(alias)).set(&current_path, &script);
+    save_alias_ini(config)?;
+
+    Ok(true)
+}
+
+pub fn delete_alias_script(alias: &str) -> Result<bool> {
+    let mut config = get_config_ini()?;
+    let current_path = pwd()?;
+
+    if let None = config.with_section(Some(alias)).get(&current_path) {
+        console(
+            State::Error,
+            format!(
+                "Can't found alias \"{}\" in this project.",
+                alias.bold().green()
+            ),
+        );
+        Ok(false)
+    } else {
+        config.with_section(Some(alias)).delete(&current_path);
+        save_alias_ini(config)?;
+        console(
+            State::Success,
+            format!("Delete alias {} success.", alias.bold().green()),
+        );
+        Ok(true)
+    }
+}
+
+pub fn show_alias(is_show_all: bool) -> Result<bool> {
+    let current_path = pwd()?;
+    let config = get_config_ini()?;
+    let mut result: HashMap<String, Vec<(String, String)>> = HashMap::new();
+
+    for (sec, prop) in config {
+        if let Some(sec) = sec {
+            for (path, script) in prop.into_iter() {
+                if is_show_all || path == current_path {
+                    result
+                        .entry(path)
+                        .or_insert(vec![])
+                        .push((sec.clone(), script));
+                }
+            }
+        }
+    }
+    if result.is_empty() {
+        if is_show_all {
+            console(State::Info, "Not found any config.")
+        } else {
+            console(State::Info, "Not found any config in this project.")
+        }
+        return Ok(false);
+    }
+
+    for (sec, scripts) in result.iter() {
+        println!("{}", format!(" {} ", sec).black().on_white());
+
+        for (alias, script) in scripts {
+            println!(" {} => {}", alias.green(), script);
+        }
+        println!();
+    }
+    Ok(true)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handle_alias_name() {
+        assert_eq!(get_alias(None::<String>), DEFAULT_SCRIPT);
+        assert_eq!(get_alias(Some("-".to_string())), DEFAULT_SCRIPT);
+        assert_eq!(get_alias(Some("start".to_string())), "start".to_string());
+    }
+
+    #[test]
+    fn find_user_script_ini() {}
 }
